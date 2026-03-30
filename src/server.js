700ec9a4deabeae95e937b405e237d8e4a62599b
Nicoly <nicoly_sousa-silva@estudante.sesisenai.org.br>
2026-03-30 09:25:56 -0300

    commit 30.03.2026

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 app.listen(3000, () => {
-  console.log('O servidor está em execução na porta 3000.');
+  console.log('O servidor está em execução em http://localhost:3000');
 });
